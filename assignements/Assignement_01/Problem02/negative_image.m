24b2c131e88005688685fe73a1108d785022007d
Taha alaswad <dr.taha.libya@gmail.com>
2024-05-31 23:44:21 +0200

    added problem 2

--- assignements/Assignement_01/Problem02/negative_image.m
+++ assignements/Assignement_01/Problem02/negative_image.m
@@ -1,15 +1,21 @@
 ################################################################################
 #Bioengineering.DIP.Spring2024.BY: Dr.Saleh Hussein.
 #Author: Taha Mahmoud.210100552. email: dr.taha.libya@gmail.com.
-#Date: 25.05.2024
+#Date: 31.05.2024
 ################################################################################
 #Assignment 01, Problem 02
 #negative image
 ################################################################################
 clc , clear all , close all
 
-J1 = imread('che.jpg');
+J1 = imread('barbara.jpg');
 J2 = 255 - J1;
+
+% Save images
+imwrite(J1,'./output/J1.jpeg');
+imwrite(J2,'./output/J2.jpeg');
+
+
 
 %Display
 figure;
